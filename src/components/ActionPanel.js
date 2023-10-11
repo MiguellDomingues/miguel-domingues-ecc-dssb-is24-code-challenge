c16811d6c777fb25e2fbfcf63779b7694d0891ef
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-10-11 13:29:55 -0700

    added cmp to display loading/err status

--- src/components/ActionPanel.js
+++ src/components/ActionPanel.js
@@ -14,7 +14,7 @@ const emptyForm =
   methodology:  METHODOLIGIES[0],    //default value of picklist                  
 }
 
-const trimStringProps = (product) => {
+const trimStringProps = (product) => { //remove white space from edges and in-between words
   return{
     ...product,
     productName: product.productName.replace(/\s+/g, ' '),
@@ -22,6 +22,7 @@ const trimStringProps = (product) => {
     scrumMasterName: product.scrumMasterName.replace(/\s+/g, ' ')}
 }
 
+//the container for all the user inputs. also displays any errors, results of actions
 function ActionPanel({
     productCount, 
     selectedProduct, 
@@ -37,7 +38,7 @@ function ActionPanel({
   
     const [actionType, setActionType] = useState(null);
 
-    function handleDelete(product){
+    function handleDelete(product){ //the confirm delete popup window
       if (window.confirm(`Delete Product ${product.productId} ?`)) {
         deleteProduct(product.productId)
       } 
@@ -53,19 +54,29 @@ function ActionPanel({
       setActionType(null);
     }
 
+    const err = "an error has occured when attempting to contact the server. Please contact your administrator" 
+
     return(<>    
-      <div className="action_panel container_style">
-        <div className="">
+      <div className="action_panel container_style">    
+
+        <span className="result_text"> {loading ? "loading....." : (error ? <span style={{color: "red"}}>{err}</span> : "")}</span>
+       
         <h1>Actions</h1>
-        {loading ? "loading....." : (error ? " an error has occured " : "")}&#160;
-        </div>
         <div className="actions container_style">
               <span>Product Count:</span>{productCount}
 
-              <button onClick={e=>{getProducts()}}  disabled={loading}>Get All Records</button>
-              <button onClick={e=>{setActionType("ADD")} } disabled={loading || error}>Add</button>
-              <button disabled={!selectedProduct || loading || error} onClick={e=>{setActionType("EDIT")}} >Edit</button>
-              <button disabled={!selectedProduct || loading || error} onClick={e=>{handleDelete(selectedProduct)}}>Delete</button>
+              <button 
+                disabled={loading} //disabled when loading
+                onClick={e=>{getProducts()}}>Get All Records</button>
+              <button 
+                disabled={loading || error} //disabled when loading or there is an error
+                onClick={e=>{setActionType("ADD")}}>Add</button>
+              <button 
+                disabled={!selectedProduct || loading || error} //disabled when loading or there is an error or the user hasnt selected anything
+                onClick={e=>{setActionType("EDIT")}}>Edit</button>
+              <button 
+                disabled={!selectedProduct || loading || error} //disabled when loading or there is an error or the user hasnt selected anything
+                onClick={e=>{handleDelete(selectedProduct)}}>Delete</button>
 
               <SearchPanel searchCB={getProductsByDeveloper} btnName={"Search Products By Developer Name"} loading={loading} error={error}/>
               <SearchPanel searchCB={getProductsByScrumMaster} btnName={"Search Products By ScrumMaster Name"} loading={loading} error={error}/>
@@ -89,27 +100,31 @@ function ActionPanel({
         : <></>)}  
   
       </div></>);
-  }
+}
   
-  function SearchPanel({ searchCB, btnName, loading, error }){
+//the search boxes for developer, scrum master text searches
+function SearchPanel({ searchCB, btnName, loading, error }){ 
   
-    const [input, setInput] = useState("");
-  
-    const handleOnChange = (e) => setInput(e.target.value)
-     
-     function handleSubmit(e){
-       e.preventDefault();
-       searchCB(input.trim());
-     }
-  
-    return (
-    <div className="search_container">
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={input} onChange={handleOnChange} required />
-        <input type="submit" value={btnName} disabled={loading || error || input.trim().length === 0}/>
-      </form>
-  </div>)
-  }
+const [input, setInput] = useState("");
 
-  export default ActionPanel;
+const handleOnChange = (e) => setInput(e.target.value)
+  
+  function handleSubmit(e){
+    e.preventDefault();
+    searchCB(input.trim());
+  }
+  
+  return (
+  <div className="search_container">
+    <form onSubmit={handleSubmit}>
+      <input type="text" value={input} onChange={handleOnChange} required />
+      <input 
+        type="submit" 
+        disabled={loading || error || input.trim().length === 0} // if we are performing a callout, there is an error, or the input are all spaces
+        value={btnName}/>
+    </form>
+</div>)
+}
+
+export default ActionPanel;
   
